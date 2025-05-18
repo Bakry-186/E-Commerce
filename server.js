b2ba7bc9b07ef6396283d5171d8d59748c572ec5
bakry <abdobakry823@gmail.com>
2025-05-18 23:47:54 +0300

    remove cors

--- server.js
+++ server.js
@@ -42,7 +42,6 @@ const limiter = rateLimit({
 
 app.use("/api", limiter);
 
-app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "uploads")));
